cdae83459373968087435647f95996d3b414ce0a
vetrovecdavid00 <111746640+vetrovecdavid00@users.noreply.github.com>
2023-03-04 00:12:06 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -52,3 +52,72 @@ document
   });
 
 weather.fetchWeather("Denver");
+
+// Get references to the search bar input and search button elements
+const searchBar = document.querySelector('.search-bar');
+const searchButton = document.querySelector('.search button');
+
+// Define a function to handle the retrieval of the user's location
+function handleLocation(position) {
+  const latitude = position.coords.latitude;
+  const longitude = position.coords.longitude;
+
+  // Use the OpenCage Geocoding API to retrieve the city name based on the coordinates
+  const apiKey = 'YOUR_API_KEY_HERE';
+  const url = `https://api.opencagedata.com/geocode/v1/json?q=${latitude}+${longitude}&key=${apiKey}`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      const city = data.results[0].components.city;
+      searchBar.value = city;
+      searchButton.click();
+    })
+    .catch(error => console.error(error));
+}
+
+// Define a function to handle errors in location retrieval
+function handleLocationError(error) {
+  console.error(error);
+}
+
+// Attach a click event listener to the search button
+searchButton.addEventListener('click', event => {
+  event.preventDefault();
+  
+  // Use the OpenWeatherMap API to retrieve weather information for the searched city
+  const apiKey = 'YOUR_API_KEY_HERE';
+  const city = searchBar.value;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      // Update the weather card with the retrieved data
+      const city = data.name;
+      const temperature = `${Math.round(data.main.temp)}°C`;
+      const icon = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+      const description = data.weather[0].description;
+      const humidity = `Humidity: ${data.main.humidity}%`;
+      const windSpeed = `Wind speed: ${data.wind.speed} km/h`;
+
+      const weatherCard = document.querySelector('.weather');
+      weatherCard.classList.remove('loading');
+      weatherCard.querySelector('.city').textContent = `Weather in ${city}`;
+      weatherCard.querySelector('.temp').textContent = temperature;
+      weatherCard.querySelector('.icon').setAttribute('src', icon);
+      weatherCard.querySelector('.description').textContent = description;
+      weatherCard.querySelector('.humidity').textContent = humidity;
+      weatherCard.querySelector('.wind').textContent = windSpeed;
+    })
+    .catch(error => console.error(error));
+});
+
+// Ask the user for their location using the Geolocation API when the page loads
+window.addEventListener('load', () => {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(handleLocation, handleLocationError);
+  } else {
+    console.error('Geolocation is not supported by this browser.');
+  }
+});
